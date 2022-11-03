b7f9d5557850db83b757c6e20f0a39d63585b269
Xaryki <109338988+Xaryki@users.noreply.github.com>
2022-11-03 22:37:40 +0300

    Add files via upload
    Задача 1 - 19 тест - TL

---
+++ Yandex Cup 2022.py
@@ -0,0 +1,32 @@
+n, m, q = map(int, input().split())
+R = {x : 0 for x in range(1, n+1)}
+datacenter = {x: [1]*m for x in range(1, n+1)}
+for i in range(q):
+    temp = input().split()
+
+    if temp[0] == "RESET":
+        datacenter[int(temp[1])] = [1] * m
+        R[int(temp[1])] += 1
+
+    elif temp[0] == "DISABLE":
+        datacenter[int(temp[1])][int(temp[2]) - 1] = 0
+
+    elif temp[0] == "GETMAX":
+        get_max = -9223372036854775807
+        maxim = {}
+        for key, values in datacenter.items():
+            if R[key] * sum(values) > get_max:
+                maxim[R[key] * sum(values)] = key
+                get_max = R[key] * sum(values)
+
+        print(min(datacenter.keys()))
+        break
+    """elif temp[0] == "GETMIN":
+        get_min = 9223372036854775807
+        mixim = {}
+        for key, items in datacenter.items():
+            if R[key] * sum(items) < get_min:
+                mixim[R[key] * sum(items)] = key
+                get_min = R[key] * sum(items)
+        print(mixim[get_min])"""
+
